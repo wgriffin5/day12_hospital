800e08a633c943528516091cc862bb251a5ad1d2
William Griffin <williamgriffin989@gmail.com>
2015-01-21 14:03:19 -0500

    update next

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,26 @@
 class PatientsController < ApplicationController
 
-def new 
+def new
+  @patient = Patient.new
+end 
+
+
+def create 
+  @patient = Patient.create patient_params
+  redirect_to root_path
 end
+
+def patient_params
+  params.require(:patient).permit(
+    :first_name,
+    :last_name,
+    :date_of_birth,
+    :description,
+    :blood_type,
+    :gender
+    )
+  end
+
 end
+
+
