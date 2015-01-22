803d4ab554c332dbd65aaba9a267afaf6b5a61ab
William Griffin <williamgriffin989@gmail.com>
2015-01-22 14:49:52 -0500

    now in patients

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -6,23 +6,39 @@ def index
 end
 
 def new
+  @hospital = Hospital.new
   end
 
+def create 
+  @hospital = Hospital.create hospital_params
+  redirect_to hospitals_path
+end 
+
+def show 
+  @hospital = Hospital.find params[:id]
+end 
+
+
+
 def edit
-  @hospital = Hospital.find params [:id]
+  @hospital = Hospital.find params[:id]
 end
 
 def update
-  @hospital
+  @hospital = Hospital.find params[:id]
+  @hospital.update_attributes hospital_params
+  redirect_to hospital_path(@hospital)
 end
 
-def destroy
-  redirect_to  
+def destroy 
+  @hospital = Hospital.find params[:id]
+  @hospital.delete
+  redirect_to hospitals_path
 end
 
   private 
   def hospital_params
-    params.require(hospital).permit
+    params.require(:hospital).permit(
     :name,
     :address,
     :city,
